424922f8d53d698d5ac36c262163e7ff888806ee
Rimonkumar <22-46957-1@student.aiub.edu>
2025-01-21 22:12:41 +0600

    comple admin

--- admin/calendar.php
+++ admin/calendar.php
@@ -4,26 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
-    <style>
-        body, html {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-            font-family: Arial, sans-serif;
-        }
-        .calendar-container {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-    </style>
+    <link rel="stylesheet" href="../staff/css/calendar.css">
+
 </head>
 <body>
     <div class="calendar-container">
